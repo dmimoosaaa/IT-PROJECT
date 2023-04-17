1d753ccfc125ba07bf74243e411d793a58acb00d
dmimoosaaa <128901244+dmimoosaaa@users.noreply.github.com>
2023-04-16 22:14:44 -0600

    Create outcomeFirstFit.py

---
+++ outcomeFirstFit.py
@@ -0,0 +1,8 @@
+Process No.	Process Size	Block No.
+1 		 212 		 2
+2 		 417 		 5
+3 		 112 		 1
+4 		 426 		 Not Allocated
+
+#In the output, the "Block No." column shows the block number to which the process has been allocated. 
+#If a process is not allocated to any block, the "Block No." value is "Not Allocated".
